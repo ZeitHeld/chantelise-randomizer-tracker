7c2fdd5cd9370d44cf9623763fd2fee63002080f
Held_der_Zeit <76132257+ZeitHeld@users.noreply.github.com>
2026-08-08 20:25:31 +0200

    item example attempt

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,10 +1,15 @@
-local item_list = {"dungeons", "events"}
+local item_list = {"dungeons", "stages", "events", "equipment", "fermin", "junk"}
 local map_list = {"world"}
-local loc_list = {"story_events"}
+local loc_list = {"story_events", "hidden_treasures"}
 local layout_list = {"items_only", "items_map", "broadcast", "maps", "tracker"}
-local layout_slim_list = {"items_map", "broadcast", "maps", "tracker"}
+local layout_core_list = {"broadcast", "item_grids"}
 
 local variant = Tracker.ActiveVariantUID
+print(variant)
+
+Tracker:AddItems("items/bt-items.json")
+Tracker:AddLayouts("layouts/bt-items.json")
+Tracker:AddLayouts("layouts/bt-ce-tracker.json")
 
 -- ITEMS
 for i, item in ipairs(item_list) do
@@ -22,14 +27,21 @@ for i, location in ipairs(loc_list) do
 end
 
 -- LAYOUTS
--- for i, layout in ipairs(layout_list) do
---     Tracker:AddLayouts("layouts/"..layout..".json")
--- end
+for i, layout in ipairs(layout_core_list) do
+    Tracker:AddLayouts("layouts/"..layout..".json")
+end
 
-if variant == "items_only" then
+-- if variant == "items_only" then
     Tracker:AddLayouts("layouts/"..variant..".json")
-else
-    for i, layout in ipairs(layout_slim_list) do
-        Tracker:AddLayouts("layouts/"..layout..".json")
-    end
+-- else
+    -- for i, layout in ipairs(layout_slim_list) do
+    --     Tracker:AddLayouts("layouts/"..layout..".json")
+    -- end
+-- end
+if variant ~= "items_only" then
+    Tracker:AddLayouts("layouts/maps.json")
+end
+
+if PopVersion and PopVersion >= "0.18.0" then
+    ScriptHost:LoadScript("scripts/autotracking.lua")
 end
